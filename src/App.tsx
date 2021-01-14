cee1250f25a078ef14e0713258248da3d83577f7
Arkadiusz Zygan <a.zygan@gmail.com>
2021-01-14 10:41:53 +0100

    Add template to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React from "react";
+import QuestionCard from "./components/QuestionCard";
 
-function App() {
+const App = () => {
+  const startTrivia = async () => {};
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+
+  const nextQuestion = () => {};
+
   return (
-    <div className="App">
-      quiz app
+    <div className='App'>
+      <h1>REACT QUIZ</h1>
+      <button className='start' onClick={startTrivia}>
+        Start
+      </button>
+      <p className="score">Score: </p>
+      <p>Loading question...</p>
+      <QuestionCard />
+      <button className="next" onClick={nextQuestion}>Next</button>
     </div>
   );
-}
+};
 
 export default App;
-
 
 // https://opentdb.com/api.php?amount=10&type=multiple // api with questions
---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,7 @@
+export interface QuestionCardProps {}
+
+const QuestionCard: React.SFC<QuestionCardProps> = () => {
+  return <div>question card</div>;
+};
+
+export default QuestionCard;
