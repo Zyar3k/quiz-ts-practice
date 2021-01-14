a67ffa286f3f4bb5afa2e1690a39f0d9a6dc442a
Arkadiusz Zygan <a.zygan@gmail.com>
2021-01-14 10:30:34 +0100

    Add lib & api link

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      quiz app
     </div>
   );
 }
 
 export default App;
+
+
+// https://opentdb.com/api.php?amount=10&type=multiple // api with questions
