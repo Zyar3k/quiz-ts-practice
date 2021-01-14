e0e753087d615d658ce402bb14f78b8abc82bb3e
Arkadiusz Zygan <a.zygan@gmail.com>
2021-01-14 11:56:44 +0100

    Show quest and anwers

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ const App = () => {
   const [number, setNumber] = useState(0);
   const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
   const [score, setScore] = useState(0);
-  const [qameOver, setGameOver] = useState(true);
+  const [gameOver, setGameOver] = useState(true);
 
   console.log(questions);
 
@@ -47,22 +47,30 @@ const App = () => {
   return (
     <div className='App'>
       <h1>REACT QUIZ</h1>
-      <button className='start' onClick={startTrivia}>
-        Start
-      </button>
-      <p className='score'>Score: </p>
-      <p>Loading question...</p>
-      {/* <QuestionCard
+      {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
+        <button className='start' onClick={startTrivia}>
+          Start
+        </button>
+      ) : null}
+      {!gameOver ? <p className='score'>Score: </p> :null}
+
+      {loading && <p>Loading question...</p>}
+      {!loading && !gameOver && (
+      <QuestionCard
         questionNr={number +1}
         totalQuestions={TOTAL_QUESTIONS}
         question={questions[number].question}
         answers={questions[number].answers}
         userAnswer={userAnswers ? userAnswers[number] : undefined}
         callback={checkAnswer}
-      /> */}
+      />
+      )}
+      {!gameOver && !loading && userAnswers.length === number + 1 && number !== TOTAL_QUESTIONS - 1 ? (
+
       <button className='next' onClick={nextQuestion}>
         Next
       </button>
+      ): null}
     </div>
   );
 };
